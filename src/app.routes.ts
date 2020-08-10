20610f728c29eff323d2eea0c32e2a255b7b791b
lean456 <leandroblanco.456@gmail.com>
2020-08-10 11:26:36 -0300

    primer commit

---
+++ src/app.routes.ts
@@ -0,0 +1,29 @@
+
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './app/components/home/home.component';
+import { OffersComponent } from './app/components/offers/offers.component';
+import { DetailsBooksComponent } from './app/components/details-books/details-books.component';
+import { ListBooksComponent } from './app/components/admin/list-books/list-books.component';
+import { LoginComponent } from './app/components/users/login/login.component';
+import { RegisterComponent } from './app/components/users/register/register.component';
+import { ProfileComponent } from './app/components/users/profile/profile.component';
+import { Page404Component } from './app/components/users/page404/page404.component';
+
+ const routes: Routes = [
+     {path:'',component: HomeComponent },
+     {path:'offers', component: OffersComponent},
+     {path: 'book/:id', component:DetailsBooksComponent},
+     {path: 'admin/list-books', component: ListBooksComponent},
+     {path: 'user/login', component: LoginComponent},
+     {path: 'user/register', component: RegisterComponent},
+     {path: 'user/profile', component: ProfileComponent},
+     {path: '**', component: Page404Component}
+    ]
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+
+export class APP_ROUTING {routes}
---
+++ src/app/components/details-books/details-books.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DetailsBooksComponent } from './details-books.component';
+
+describe('DetailsBooksComponent', () => {
+  let component: DetailsBooksComponent;
+  let fixture: ComponentFixture<DetailsBooksComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DetailsBooksComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DetailsBooksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
