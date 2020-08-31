ce6a6b5aad63d3420ffb41a2b4fcdb2d21217e44
lean456 <leandroblanco.456@gmail.com>
2020-08-31 10:22:20 -0300

    resolve heroku

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-console.log(__dirname);
 app.use(express.static(__dirname + '/dist/firebase'));
-app.get('/*', function(req,res) {
+
+app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/firebase/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
